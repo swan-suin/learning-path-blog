7334e64ce6cbc27b112031533ee86f46b161a790
SANG HYUK CHUNG <chris@SANGs-MacBook-Air-2736.local>
2025-04-12 19:33:28 +0900

    Create our basic blog

---
+++ config/importmap.rb
@@ -0,0 +1,16 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@tailwindcss/typography", to: "@tailwindcss--typography.js" # @0.5.16
+pin "cssesc" # @3.0.0
+pin "lodash.castarray" # @4.4.0
+pin "lodash.isplainobject" # @4.0.6
+pin "lodash.merge" # @4.6.2
+pin "postcss-selector-parser" # @6.0.10
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.3
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.3
+pin "util-deprecate" # @1.0.2
